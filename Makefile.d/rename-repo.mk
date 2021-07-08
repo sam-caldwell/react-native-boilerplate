e958321fa7196580cfa5daedd89dc592c1f80dbe
Sam Caldwell <github@samcaldwell.net>
2021-07-07 22:44:26 -0500

    working to fix rename-repo

--- Makefile.d/rename-repo.mk
+++ Makefile.d/rename-repo.mk
@@ -4,14 +4,15 @@ rename-repo:
 	@echo "rename-repo starting"
 	@(\
 		read -p "Enter the name of your project: " REPO_NAME;\
-		echo "creating {$REPO_NAME}";\
+		echo "creating '$${REPO_NAME}'";\
 		cd ..;\
-		mv react-native-boilerplate ${REPO_NAME};\
-		cd ${REPO_NAME};\
+		cp -rfvp react-native-boilerplate $${REPO_NAME};\
+		cd $${REPO_NAME};\
 		rm -rf .git;\
+		git config --global init.defaultBranch main;\
 		git init .;\
-		echo "a new local git repo is created for ${REPO_NAME}";\
+		echo "a new local git repo is created for '$${REPO_NAME}'";\
 		git add -A .;\
 		git commit -m "initial commit";\
-		echo "You'll need to create the repo for ${REPO_NAME} in github/bitbucket and push";\
+		echo "You'll need to create the repo for $${REPO_NAME} in github/bitbucket and push";\
 	)
