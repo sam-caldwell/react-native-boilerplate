d984f55dd69b5fd311f5be78e63436718e7b1a33
Sam Caldwell <github@samcaldwell.net>
2021-07-07 22:45:55 -0500

    simplify the rename-repo bootstrap.

--- Makefile.d/rename-repo.mk
+++ Makefile.d/rename-repo.mk
@@ -2,17 +2,16 @@
 
 rename-repo:
 	@echo "rename-repo starting"
-	@(\
-		read -p "Enter the name of your project: " REPO_NAME;\
-		echo "creating '$${REPO_NAME}'";\
-		cd ..;\
-		cp -rfvp react-native-boilerplate $${REPO_NAME};\
-		cd $${REPO_NAME};\
-		rm -rf .git;\
-		git config --global init.defaultBranch main;\
-		git init .;\
-		echo "a new local git repo is created for '$${REPO_NAME}'";\
-		git add -A .;\
-		git commit -m "initial commit";\
-		echo "You'll need to create the repo for $${REPO_NAME} in github/bitbucket and push";\
-	)
+	read -p "Enter the name of your project: " REPO_NAME
+	echo "creating '$${REPO_NAME}'"
+	cd ..
+	cp -rfvp react-native-boilerplate $${REPO_NAME}
+	cd $${REPO_NAME}
+	rm -rf .git
+	git config --global init.defaultBranch main
+	git init .
+	echo "a new local git repo is created for '$${REPO_NAME}'"
+	git add -A .
+	git commit -m "initial commit"
+	echo "You'll need to create the repo for $${REPO_NAME} in github/bitbucket and push"
+
